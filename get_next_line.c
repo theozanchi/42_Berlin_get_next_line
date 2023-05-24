18e510599379c5ba32271ec90f42558d0e20ec93
tzanchi <tzanchi@student.42berlin.de>
2023-05-24 13:24:04 +0200

    fix of norminette error on extract_line function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 16:31:02 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/05/24 12:31:32 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/05/24 13:19:30 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ char	*extract_line(char **archive)
 {
 	size_t	length;
 	char	*line;
-	char	*linebreak_position;
 	char	*temp;
 
-	linebreak_position = ft_strchr(*archive, '\n');
-	if (linebreak_position == NULL)
+	if (ft_strchr(*archive, '\n') == NULL)
 	{
 		length = ft_strlen(*archive);
 		line = malloc((length + 1) * sizeof(char));
@@ -28,16 +26,15 @@ char	*extract_line(char **archive)
 			return (NULL);
 		ft_strlcpy(line, *archive, length);
 		free(*archive);
-		*archive = NULL;
 	}
 	else
 	{
-		length = linebreak_position - *archive + 1;
+		length = ft_strchr(*archive, '\n') - *archive + 1;
 		line = malloc((length + 1) * sizeof(char));
 		if (line == NULL)
 			return (NULL);
 		ft_strlcpy(line, *archive, length + 1);
-		temp = ft_strjoin(linebreak_position + 1, "");
+		temp = ft_strjoin(ft_strchr(*archive, '\n') + 1, "");
 		free(*archive);
 		*archive = temp;
 	}
@@ -55,7 +52,11 @@ char	*get_next_line(int fd)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE - 1);
 		if (bytes_read <= 0)
+		{
+			if (archive != NULL)
+				free(archive);
 			return (NULL);
+		}
 		buffer[bytes_read] = '\0';
 		archive = ft_strjoin("", buffer);
 		if (archive == NULL)
