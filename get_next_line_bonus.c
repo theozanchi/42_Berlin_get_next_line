bf2f0a5bad2da5a8a5d29ca5b79eb62c6998b5a3
tzanchi <tzanchi@student.42berlin.de>
2023-05-26 17:38:49 +0200

    add bonus files

---
+++ get_next_line_bonus.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 16:31:02 by tzanchi           #+#    #+#             */
+/*   Updated: 2023/05/26 17:37:21 by tzanchi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+/*free_archive frees the memory allocated to archive, sets it to NULL and
+returns NULL*/
+static char	*free_archive(char **archive)
+{
+	if (*archive)
+	{
+		free(*archive);
+		*archive = NULL;
+	}
+	return (NULL);
+}
+
+/*join_archive_and_buffer is joining archive and the buffer and properly
+freeing the previously allocated memory*/
+static void	join_archive_and_buffer(char **archive, char *buffer)
+{
+	char	*temp;
+
+	temp = ft_strjoin(*archive, buffer);
+	free_archive(archive);
+	*archive = temp;
+}
+
+/*extract_line looks the first occurence of '\n' in archive and returns all the
+characters before it, it then updates archive by deleting all the characters
+before '\n', and '\n' itself.
+If no '\n'' is found in archive, extrcat_line returns archive and frees it*/
+static char	*extract_line(char **archive)
+{
+	size_t	length;
+	char	*line;
+	char	*temp;
+
+	if (ft_strchr(*archive, '\n'))
+		length = ft_strchr(*archive, '\n') - *archive + 1;
+	else
+		length = ft_strlen(*archive);
+	line = malloc((length + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, *archive, length + 1);
+	if (ft_strchr(*archive, '\n'))
+	{
+		temp = ft_strjoin(ft_strchr(*archive, '\n') + 1, "");
+		free_archive(archive);
+		*archive = temp;
+	}
+	else
+		free_archive(archive);
+	return (line);
+}
+
+/*get_next_line reads up to BUFFER_SIZE bytes of the file pointed by fd into 
+buffer. buffer is stored into the static variable archive at each iteration,
+and the line is extracted from archive by the extract_line function
+get_next_line returns the current line at each linebreak it encounters*/
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	*archive[FD_MAX] = {NULL};
+	ssize_t		bytes_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX - 1)
+		return (NULL);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		buffer[bytes_read] = '\0';
+		if (!archive[fd])
+			archive[fd] = ft_strjoin("", buffer);
+		else
+			join_archive_and_buffer(&archive[fd], buffer);
+		if (ft_strchr(archive[fd], '\n'))
+			break ;
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	}
+	free(buffer);
+	if (bytes_read == -1 || !archive[fd] || !*archive[fd])
+		return (free_archive(&archive[fd]));
+	return (extract_line(&archive[fd]));
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 16:30:56 by tzanchi           #+#    #+#             */
+/*   Updated: 2023/05/26 17:36:58 by tzanchi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# ifndef FD_MAX
+#  define FD_MAX 1024
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int c);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+
+#endif
